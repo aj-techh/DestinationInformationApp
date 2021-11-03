24fecda8b7633e0280e93f1235d465089e1ecdc8
aj_techh <aj_techh@pm.me>
2021-11-03 01:12:12 +0000

    gave ifram title

--- frontenddestinationinformationapp/src/components/components/DestinationDisplay.js
+++ frontenddestinationinformationapp/src/components/components/DestinationDisplay.js
@@ -71,7 +71,7 @@ const DestinationDisplay = (props) => {
                     {/* <span id="map">{destination}</span> */}
                     <iframe
                         // className="mw-100"
-                        // title="dispMap"
+                        title="dispMap"
                         width="100%"
                         height="100%"
                         // frameborder="0" style={{border:'0'}}
